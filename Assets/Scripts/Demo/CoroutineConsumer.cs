7c43eb34778e84393e29186e5047d968ed38083f
AkiraWatanabe23 <a0323w@gmail.com>
2023-10-29 16:11:56 +0900

    [update] Coroutine, Task

---
+++ Assets/Scripts/Demo/CoroutineConsumer.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading;
+using UnityEngine;
+
+public class CoroutineConsumer : MonoBehaviour
+{
+    [SerializeField]
+    private CoroutineProvider _provider = default;
+
+    private readonly CancellationTokenSource _ctsA = new();
+    private readonly CancellationTokenSource _ctsB = new();
+
+    private readonly CancelledData _cancelledA = new();
+    private readonly CancelledData _cancelledB = new();
+
+    private void Start()
+    {
+        _provider.Run("処理1", _ctsA.Token, _cancelledA);
+        _provider.Run("処理2", _ctsB.Token, _cancelledB);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A)) { _ctsA.Cancel(); }
+        if (Input.GetKeyDown(KeyCode.B)) { _ctsB.Cancel(); }
+    }
+
+    private void OnDestroy()
+    {
+        _ctsA.Dispose();
+        _ctsB.Dispose();
+    }
+}
+
+public class CancelledData
+{
+    /// <summary> 終了時の時間 </summary>
+    private float _cancelledTime = 0f;
+    /// <summary> 動的なCancel命令が入ったか </summary>
+    private bool _isCancelOrder = false;
+
+    /// <summary> Cancel命令が行われたかどうか </summary>
+    public bool IsGetCancelOrder => _isCancelOrder;
+
+    public void SetResultData(float time)
+    {
+        _cancelledTime = time;
+        _isCancelOrder = true;
+
+        Debug.Log($"CancelTime={_cancelledTime}");
+    }
+}
---
+++ Assets/Scripts/Demo/CoroutineProvider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Threading;
+using UnityEngine;
+
+public class CoroutineProvider : MonoBehaviour
+{
+    /// <summary> 外から、何か継続的な処理の開始を指示する。 </summary>
+    public Coroutine Run(string name, CancellationToken token, CancelledData cancelled)
+    {
+        return StartCoroutine(RunCoroutine(name, token, cancelled));
+    }
+
+    private IEnumerator RunCoroutine(string name, CancellationToken token, CancelledData cancelled)
+    {
+        var elapsed = 0f;
+
+        while (!token.IsCancellationRequested && elapsed <= 5f)
+        {
+            elapsed += Time.deltaTime;
+            // 何か継続処理...
+            Debug.Log($"{name}: {Time.frameCount}");
+            yield return null;
+        }
+        Debug.Log($"{name}が終了しました");
+
+        cancelled.SetResultData(elapsed);
+    }
+}
